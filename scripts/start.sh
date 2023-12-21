a8992c3a2eaf3b748ff73c81152f1bdc06ef8594
ZaharKondaurov <stallmens@yandex.ru>
2023-12-22 01:36:30 +0300

    some fixes in requests.txt and sleep interval

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,4 +5,6 @@ docker-compose -f ./environment/docker-compose.yaml down --remove-orphans -v
 docker-compose -f ./environment/docker-compose.yaml build --no-cache
 docker-compose -f ./environment/docker-compose.yaml up -d
 sleep 5
+# sleep 30
+# ^ option for Zakhar....
 python3 ./logic/simulate.py
