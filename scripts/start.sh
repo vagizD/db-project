b20aa26bd4414b0a09c3db0f5bce34b9ff0251ed
vagiz.daudov <vagiz.0282@gmail.com>
2023-12-22 10:56:04 +0300

    Comment change

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,7 +4,6 @@ echo '---- START DOCKER ----'
 docker-compose -f ./environment/docker-compose.yaml down --remove-orphans -v
 docker-compose -f ./environment/docker-compose.yaml build --no-cache
 docker-compose -f ./environment/docker-compose.yaml up -d
+# increase sleep-time if required
 sleep 5
-# sleep 30
-# ^ option for Zakhar....
 python3 ./logic/simulate.py
